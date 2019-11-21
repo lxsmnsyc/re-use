2b32db86e9595c222b23320f7db356480b5ce4d8
Alexis Munsayac <alexis@lyon.com.ph>
2019-11-21 12:58:38 +0800

    Update useIsomorphicEffect.js

--- src/bindings/useIsomorphicEffect.js
+++ src/bindings/useIsomorphicEffect.js
@@ -1,8 +1,9 @@
 var React = require('react');
 
-var useIsomorphicEffect = typeof window === 'undefined'
-  ? React.useEffect
-  : React.useLayoutEffect
-;
+var useIsomorphicEffect = function (effect, deps) {
+  (typeof window === 'undefined'
+    ? React.useEffect
+    : React.useLayoutEffect)(effect, deps)
+}
 
 module.export = useIsomorphicEffect;
