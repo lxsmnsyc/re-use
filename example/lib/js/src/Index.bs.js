2d99796eb110ceadf012608872642849232026ce
Alexis Munsayac <alexis@lyon.com.ph>
2019-12-13 14:53:37 +0800

    Update Index.bs.js

--- example/lib/js/src/Index.bs.js
+++ example/lib/js/src/Index.bs.js
@@ -4,8 +4,6 @@ var React = require("react");
 var ReactDOMRe = require("reason-react/lib/js/src/ReactDOMRe.js");
 var ReasonReactRouter = require("reason-react/lib/js/src/ReasonReactRouter.js");
 var Constant$ReasonReactExamples = require("./Constant.bs.js");
-var EffectOnce$ReasonReactExamples = require("./EffectOnce.bs.js");
-var ConstantCallback$ReasonReactExamples = require("./ConstantCallback.bs.js");
 
 function Index$Nav(Props) {
   return React.createElement("ul", undefined, React.createElement("li", undefined, React.createElement("a", {
@@ -13,8 +11,8 @@ function Index$Nav(Props) {
                     }, "Constant Demo")), React.createElement("li", undefined, React.createElement("a", {
                       href: "/#/constant-callback"
                     }, "ConstantCallback Demo")), React.createElement("li", undefined, React.createElement("a", {
-                      href: "/#/effect-once"
-                    }, "EffectOnce Demo")));
+                      href: "/#/native-state"
+                    }, "NativeState Demo")));
 }
 
 var Nav = {
@@ -23,16 +21,12 @@ var Nav = {
 
 function Index$App(Props) {
   var url = ReasonReactRouter.useUrl(undefined, /* () */0);
-  var match = url[/* hash */1];
+  var match = url.hash;
   switch (match) {
     case "/" :
         return React.createElement(Index$Nav, { });
     case "/constant" :
         return React.createElement(Constant$ReasonReactExamples.Demo.make, { });
-    case "/constant-callback" :
-        return React.createElement(ConstantCallback$ReasonReactExamples.Demo.make, { });
-    case "/effect-once" :
-        return React.createElement(EffectOnce$ReasonReactExamples.Demo.make, { });
     default:
       return React.createElement(Index$Nav, { });
   }
