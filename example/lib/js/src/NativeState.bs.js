213f4161a85b97a898eef9368fc35f97e4598db5
Alexis Munsayac <alexis@lyon.com.ph>
2019-12-13 14:53:41 +0800

    Create NativeState.bs.js

---
+++ example/lib/js/src/NativeState.bs.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var Curry = require("bs-platform/lib/js/curry.js");
+var React = require("react");
+var NativeState$ReUse = require("@lxsmnsyc/re-use/lib/js/src/NativeState.bs.js");
+
+function NativeState$Demo(Props) {
+  var match = NativeState$ReUse.call((function (param) {
+          return false;
+        }));
+  var setState = match[1];
+  var state = match[0];
+  return React.createElement("button", {
+              onClick: (function (param) {
+                  return Curry._1(setState, (function (param) {
+                                return !state;
+                              }));
+                })
+            }, state ? "True" : "False");
+}
+
+var Demo = {
+  make: NativeState$Demo
+};
+
+exports.Demo = Demo;
+/* react Not a pure module */
